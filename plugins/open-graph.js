1a031a640ad496e766e42ef06cdc7469d13b94a6
Miriam Suzanne <miriam@oddbird.net>
2023-03-10 23:57:52 -0700

    Simplify og image for now

--- plugins/open-graph.js
+++ plugins/open-graph.js
@@ -2,13 +2,13 @@ const pluginRss = require('@11ty/eleventy-plugin-rss');
 
 const ogImage = (og, url, cache) => {
   if (og && og.image) {
-    return `/images/${og.image}/_${cache || new Date().toISOString()}`;
+    return `/images/${og.image}/`;
   }
 
   const api = 'https://screenshot-api.miriam.codes/';
   const baseUrl = process.env.URL || 'http://localhost:8080/';
-  const encoded = encodeURIComponent(`${baseUrl}/_og${url}`);
-  return `${api}${encoded}/opengraph/`;
+  const encoded = encodeURIComponent(`${baseUrl}/${url}`);
+  return `${api}${encoded}/opengraph/_${cache || new Date().toISOString()}`;
 }
 
 module.exports = function (eleventyConfig) {
