7a45cce1881674f4a7ff09b0bd18e074424305eb
Miriam Suzanne <miriam@oddbird.net>
2023-03-12 01:03:46 -0700

    Point open graph image at generated page

--- plugins/open-graph.js
+++ plugins/open-graph.js
@@ -7,7 +7,7 @@ const ogImage = (og, url) => {
 
   const api = 'https://screenshot-api.miriam.codes/';
   const baseUrl = process.env.URL || 'http://localhost:8080/';
-  const encoded = encodeURIComponent(`${baseUrl}/${url}`);
+  const encoded = encodeURIComponent(`${baseUrl}/_og${url}`);
   return `${api}${encoded}/opengraph/_wait:2_${new Date().toISOString()}`;
 }
 
