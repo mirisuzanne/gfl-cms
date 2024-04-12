d769f56a7041a601987637d4a596f5496cd3bad5
Miriam Suzanne <miriam@oddbird.net>
2024-04-11 23:16:54 -0600

    Tell coda that I want UTC dates

--- plugins/time.js
+++ plugins/time.js
@@ -1,15 +1,14 @@
 const toFormat = require('date-fns/format');
-const { utcToZonedTime, toDate } = require('date-fns-tz');
+const { utcToZonedTime } = require('date-fns-tz');
 
 const year = (date) => `${new Date(date).getFullYear()}`;
-const zone = 'America/Denver';
 
 module.exports = function (eleventyConfig, formats = {}) {
   // https://date-fns.org/v2.21.2/docs/format
   const formatDate = (date, format = 'default') => {
-    const dateObj = toDate(date, zone);
+    const utcDate = utcToZonedTime(date, '+00:00');
     const useFormat = formats[format] || format;
-    const inFormat = toFormat(dateObj, useFormat, zone);
+    const inFormat = toFormat(utcDate, useFormat);
     return inFormat;
   };
 
